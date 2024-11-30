c09b275e00cf39e65e77c55d84d58595da6f5aec
Eliza Zhang <eliza@elizazhang.com>
2024-11-30 04:00:55 -0800

    Implement random play

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@ use std::{iter::repeat, net::TcpStream};
 use clap::Parser;
 use rand::seq::SliceRandom as _;
 use shengji::serving_types::{JoinRoom, UserMessage};
-use shengji_core::{game_state::GameState, interactive::Action};
-use shengji_mechanics::{ordered_card::OrderedCard, trick::UnitLike, types::Card};
+use shengji_core::{
+    game_state::{self, GameState},
+    interactive::Action,
+};
+use shengji_mechanics::{ordered_card::OrderedCard, trick::UnitLike, types::PlayerID};
 use shengji_types::{GameMessage, ZSTD_ZSTD_DICT};
-use tracing::{debug, info, instrument, span, trace, Level};
+use tracing::{debug, info, instrument, trace};
 use tracing_subscriber::{
     fmt::format::FmtSpan, layer::SubscriberExt as _, util::SubscriberInitExt as _,
 };
@@ -49,244 +52,148 @@ fn main() {
     socket.chat(
         "Beep boop, I'm a bot! I just joined, so please give me a moment to get my bearings.",
     );
+    let bot_player_id = game_state.player_id(&argv.name).unwrap();
 
-    // Start in the Initialize phase.
-    //
-    // TODO: I should just handle these phases statelessly, since I get the
-    // whole game state for every phase anyway.
-    let span = span!(Level::INFO, "initialize_phase");
-    let me = {
-        let _enter = span.enter();
-        let me = if let GameState::Initialize(i) = game_state {
-            socket.send(UserMessage::Ready);
-            i.players()
-                .iter()
-                .find(|p| p.name == argv.name)
-                .unwrap()
-                .clone()
-        } else {
-            socket.chat(
-                "It looks like the game has already started. I don't know how to handle this situation yet. Disconnecting!"
-            );
-            panic!("Expected Initialize phase, but got: {:#?}", game_state);
-        };
-        info!(?me, "Bot player");
-
-        // Wait until we move onto the Draw phase. This might take a few state
-        // updates, since each settings change in the Initialize phase results in a
-        // state update.
-        loop {
-            game_state = socket.read_state();
-            if let GameState::Draw(_) = game_state {
-                break;
-            } else if let GameState::Initialize(_) = game_state {
-                debug!(?game_state, "Waiting for Draw phase");
-            } else {
-                panic!(
-                    "Unexpected state during Initialize phase: {:#?}",
-                    game_state
-                );
-            }
-        }
-        me
-    };
-
-    // In the Draw phase, draw cards when it's the bot's turn. Otherwise, wait
-    // until the Exchange phase. This bot currently never bids.
-    let span = span!(Level::INFO, "draw_phase");
-    {
-        let _enter = span.enter();
-        info!(?game_state, "Entering Draw phase");
-        loop {
-            if let GameState::Draw(p) = game_state {
-                match p.next_player() {
-                    Ok(p) => {
-                        trace!(?p, "Next player to draw");
-                        if p == me.id {
-                            debug!("Drawing card");
-                            socket.send(UserMessage::Action(Action::DrawCard));
-                        } else {
-                            debug!(next_player_id = ?p, "Waiting for next player to draw");
-                        }
-                    }
-                    Err(e) => {
-                        if e.to_string() == "nobody has bid yet" {
-                            debug!("Waiting for bids to be made")
-                        } else {
-                            panic!("Unexpected error during Draw phase: {}", e)
-                        }
-                    }
-                }
-            } else if let GameState::Exchange(_) = game_state {
-                break;
-            } else {
-                panic!("Unexpected state during Draw phase: {:#?}", game_state);
-            }
-            game_state = socket.read_state();
-            trace!(?game_state, "Updated state");
-        }
-    }
-
-    // In the Exchange phase, do nothing, since the bot will never have the bid.
-    let span = span!(Level::INFO, "exchange_phase");
-    {
-        let _enter = span.enter();
-        info!(?game_state, "Entering Exchange phase");
-        loop {
-            game_state = socket.read_state();
-            if let GameState::Play(_) = game_state {
-                break;
-            } else if let GameState::Exchange(_) = game_state {
-                debug!(?game_state, "Waiting for Play phase");
-            } else {
-                panic!("Unexpected state during Exchange phase: {:#?}", game_state);
-            }
-        }
-    }
-
-    // In the Play phase, the bot plays random valid tricks.
-    let span = span!(Level::INFO, "play_phase");
-    let _enter = span.enter();
-    info!(?game_state, "Entering Play phase");
     loop {
-        if let GameState::Play(ref p) = game_state {
-            let trick = p.trick();
-            debug!(?trick, "Current trick");
+        match game_state {
+            // Do nothing, and wait for the game to start.
+            GameState::Initialize(_) => trace!("Waiting for game to start"),
 
-            let played_cards = trick.played_cards();
-            debug!(?played_cards, "Currently played cards");
-
-            match trick.next_player() {
-                Some(next_player_id) => {
-                    if next_player_id != me.id {
-                        debug!(?next_player_id, "Waiting for next player to play");
+            // Draw cards on the bot's turn, but never bid.
+            //
+            // TODO: Implement bidding logic.
+            GameState::Draw(draw_phase) => match draw_phase.next_player() {
+                Ok(next_player_id) => {
+                    if next_player_id == bot_player_id {
+                        debug!("Drawing card");
+                        socket.send(UserMessage::Action(Action::DrawCard));
                     } else {
-                        debug!("Playing trick");
-                        let settings = p.propagated();
-                        let hands = p.hands();
-                        let current_hand_counts = hands.get(me.id).unwrap();
-                        debug!(?current_hand_counts, "Current hand");
-                        let current_hand = current_hand_counts
-                            .iter()
-                            .flat_map(|(card, count)| repeat(*card).take(*count))
-                            .collect::<Vec<_>>();
-
-                        match trick.trick_format() {
-                            None => {
-                                assert!(played_cards.len() == 0);
-                                debug!("Starting a new trick");
-
-                                // For now, just play a random card. Any one-card
-                                // starting play will always be valid.
-                                let card = current_hand.choose(&mut rand::thread_rng()).unwrap();
-                                debug!(?card, "Playing card");
-                                socket.send(UserMessage::Action(Action::PlayCards(vec![*card])));
-                            }
-                            Some(trick_format) => {
-                                assert!(played_cards.len() > 0);
-                                debug!(?trick_format, "Following this trick format");
-
-                                let cards_in_trick_suit = current_hand
-                                    .iter()
-                                    .filter(|c| {
-                                        trick_format.trump().effective_suit(**c)
-                                            == trick_format.suit()
-                                    })
-                                    .copied()
-                                    .collect::<Vec<_>>();
-
-                                let matching_play = trick_format
-                                    .decomposition(settings.trick_draw_policy())
-                                    .filter_map(|format| {
-                                        let mut playable = UnitLike::check_play(
-                                            OrderedCard::make_map(
-                                                current_hand.iter().copied(),
-                                                trick_format.trump(),
-                                            ),
-                                            format.iter().cloned(),
-                                            settings.trick_draw_policy(),
-                                        );
-
-                                        playable.next().map(|units| {
-                                            units
-                                                .iter()
-                                                .flat_map(|unit| {
-                                                    unit.iter().flat_map(|(card, count)| {
-                                                        repeat(card.card).take(*count)
-                                                    })
-                                                })
-                                                .collect::<Vec<_>>()
-                                        })
-                                    })
-                                    .next();
-
-                                // TODO: I took this from `core/examples/simulate_play.rs`.
-                                // How the hell does this work?
-                                //
-                                // TODO: FIXME: I don't think this _does_ work. I think I need to write my own logic for this.
-                                let num_required = trick_format.size();
-                                let mut play = match matching_play {
-                                    Some(matching) if matching.len() == num_required => matching,
-                                    Some(_) if num_required >= cards_in_trick_suit.len() => {
-                                        cards_in_trick_suit
-                                    }
-                                    Some(mut matching) => {
-                                        // There are more available cards than required; we must at least
-                                        let mut available_cards = cards_in_trick_suit;
-                                        // pick the matching. Do this inefficiently!
-                                        for m in &matching {
-                                            available_cards.remove(
-                                                available_cards
-                                                    .iter()
-                                                    .position(|c| *c == *m)
-                                                    .unwrap(),
-                                            );
-                                        }
-                                        available_cards.shuffle(&mut rand::thread_rng());
-                                        matching.extend(
-                                            available_cards[0..num_required - matching.len()]
-                                                .iter()
-                                                .copied(),
-                                        );
-
-                                        matching
-                                    }
-                                    None => cards_in_trick_suit,
-                                };
-                                let required_other_cards = num_required - play.len();
-                                if required_other_cards > 0 {
-                                    let mut other_cards =
-                                        Card::cards(current_hand_counts.iter().filter(|(c, _)| {
-                                            trick_format.trump().effective_suit(**c)
-                                                != trick_format.suit()
-                                        }))
-                                        .copied()
-                                        .collect::<Vec<_>>();
-                                    other_cards.shuffle(&mut rand::thread_rng());
-                                    play.extend(
-                                        other_cards[0..required_other_cards].iter().copied(),
-                                    );
-                                }
-
-                                debug!(?play, "Playing cards");
-                                socket.send(UserMessage::Action(Action::PlayCards(play)));
-                            }
-                        }
+                        debug!(?next_player_id, "Waiting for next player to draw");
                     }
                 }
-                None => {
-                    // This happens when the trick has ended (i.e. been won by
-                    // somebody), but nobody has moved on to the next trick yet
-                    // (i.e. hit the "Finish Trick" button, which sends the
-                    // "EndTrick" action).
-                    debug!("Waiting for next trick");
+                Err(e) => {
+                    if e.to_string() == "nobody has bid yet" {
+                        debug!("Waiting for bids to be made")
+                    } else {
+                        panic!("Unexpected error during Draw phase: {}", e)
+                    }
+                }
+            },
+
+            // Do nothing. Since the bot never bids, it will never have to
+            // exchange cards.
+            GameState::Exchange(_) => trace!("Waiting for landlord to exchange cards"),
+
+            // Play valid tricks at random.
+            GameState::Play(play_phase) => {
+                let trick = play_phase.trick();
+                debug!(?trick, "Current trick");
+                match trick.next_player() {
+                    Some(next_player_id) => {
+                        if next_player_id == bot_player_id {
+                            debug!("Playing trick");
+                            play_trick(&mut socket, &play_phase, bot_player_id);
+                        } else {
+                            debug!(?next_player_id, "Waiting for next player to play");
+                        }
+                    }
+                    None => {
+                        // This happens when the trick has ended (i.e. been won
+                        // by somebody), but nobody has moved on to the next
+                        // trick yet (i.e. hit the "Finish Trick" button, which
+                        // sends the "EndTrick" action).
+                        debug!("Waiting for next trick");
+                    }
                 }
             }
-        } else {
-            panic!("Unexpected state during Play phase: {:#?}", game_state);
         }
         game_state = socket.read_state();
-        trace!(?game_state, "Updated state");
+        trace!(?game_state, "New game state");
+    }
+}
+
+#[instrument(level = "trace", skip(socket))]
+fn play_trick(
+    socket: &mut ShengjiSocket,
+    play_phase: &game_state::play_phase::PlayPhase,
+    bot_player_id: PlayerID,
+) {
+    let trick = play_phase.trick();
+    let played_cards = trick.played_cards();
+    let hands = play_phase.hands();
+    let bot_hand = hands.get(bot_player_id).unwrap();
+    debug!(?bot_hand, "Current hand");
+    let bot_hand_cards = bot_hand
+        .iter()
+        .flat_map(|(card, count)| repeat(*card).take(*count))
+        .collect::<Vec<_>>();
+
+    match trick.trick_format() {
+        None => {
+            assert!(played_cards.len() == 0);
+            debug!("Starting a new trick");
+
+            // For now, just play a random card. Any one-card starting play will
+            // always be valid.
+            let card = bot_hand_cards.choose(&mut rand::thread_rng()).unwrap();
+            debug!(?card, "Playing card");
+            socket.send(UserMessage::Action(Action::PlayCards(vec![*card])));
+        }
+        Some(trick_format) => {
+            assert!(played_cards.len() > 0);
+            debug!(?trick_format, "Following trick format");
+
+            let trick_draw_policy = play_phase.propagated().trick_draw_policy();
+            let cards_in_trick_suit = bot_hand_cards
+                .iter()
+                .filter(|c| trick_format.trump().effective_suit(**c) == trick_format.suit())
+                .copied()
+                .collect::<Vec<_>>();
+
+            let plays_within_suit = trick_format
+                .decomposition(trick_draw_policy)
+                .map(|format| {
+                    UnitLike::check_play(
+                        OrderedCard::make_map(
+                            cards_in_trick_suit.iter().copied(),
+                            trick_format.trump(),
+                        ),
+                        format.iter().cloned(),
+                        trick_draw_policy,
+                    )
+                    .map(|units| {
+                        units
+                            .into_iter()
+                            .flat_map(|u| {
+                                u.into_iter()
+                                    .flat_map(|(card, count)| repeat(card.card).take(count))
+                                    .collect::<Vec<_>>()
+                            })
+                            .collect::<Vec<_>>()
+                    })
+                    .collect::<Vec<_>>()
+                })
+                .flatten()
+                .collect::<Vec<_>>();
+
+            debug!(?plays_within_suit, "Possible plays within the suit");
+
+            // TODO: For truly random behavior, group the `plays_within_suit` by
+            // Ord so that we know the set of hands with highest value (which
+            // are the set we are required to pick from), and then pick randomly
+            // among that set.
+            let play = match plays_within_suit.first() {
+                Some(p) => p,
+                // If there are no plays available within the suit, then all
+                // plays are valid.
+                None => &bot_hand_cards
+                    .choose_multiple(&mut rand::thread_rng(), trick_format.size())
+                    .copied()
+                    .collect(),
+            };
+            debug!(?play, "Playing cards");
+            socket.send(UserMessage::Action(Action::PlayCards(play.clone())));
+        }
     }
 }
 
